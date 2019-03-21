6d28a0980b41b6f59b7d06f6d599fcb5a243c03a
Markus Maasikas <markus@hotmail.com>
2019-03-21 07:31:42 -0400

    3.21

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,64 +8,196 @@ $(document).ready(function () {
         id: 'questions-one',
         answer: 1
 
+      }, {
+        question: 'What is my last name?',
+        possibles: ['Smith', 'Maasikas', 'Tyler'],
+        id: 'question-two',
+        answer: 2
+
       }
 
-    ]};
+
+    ]
+  };
 
 
-
-  //console.log(question);
+  // var text = "";
+  // $("#doneButton").hide();
 
   //var message = "Game Over!";
   //sometype of jquery function to show this
 
-  $('.startGame').on('click', function () {
-    //console.log("click");
-    //wrapper or questions-container
-    $('#questions-container').show();
-    console.log('hello');
+
+
+  //    var question = $("<div>").addClass("card");
+  // var qcard = $("<div>").addClass("card-body");
+  // var possibles = $("<ul>").addClass("list-group-flush list-group");
+
+  // qcard.html("<h5 class='card-title'>"+ game.questions[0].question+ "</h5>");
+  // // possibles =
+  // for (i = 0; i < game.questions.length; i++) {
+  // $("#questions-container").append("<li>"+ game.questions[i].question + "</li>");
+  // }
+
+  // for (i = 0; i < game.possibles.length; i++){
+  // $("#questions-contaier").append("<li>" + game.possibles[i].possibles + "</li>");
+
+  //  // text += "<li>" + game.questions[i] +"</li>";
+  // }
+
+  // //  <div class="card" style="width: 18rem;">
+  // //   <img src="..." class="card-img-top" alt="...">
+  // //  <div class="card-body">
+  // //  <h5 class="card-title">Totally Trivial Trivia</h5>
+  // //  <p class="card-text">questions or title.</p>
+  // //   </div>
+  // //  <ul class="list-group list-group-flush">
+  // //  <li class="list-group-item"> </li>
+  // //  <li class="list-group-item"> </li>
+  // //  <li class="list-group-item"> </li>
+  // //  </ul>
+  // //  </div>
+  // //  </div>
+  // // };
+  //wrapper or questions-container
+  // $("#questions-container").append(question);
+  // $('#questions-container').show();
+  //console.log('he');
+  $(".startGame").on("click", function () {
+    $('.wrapper').show();
+    console.log('click');
     $(this).hide();
 
-     });
+  });
 
-  var number = 30;
-  $("#timeLeft").on()('click', run);
+  var number = 40;
+  $("#timeLeft").on('click', run);
 
   function decrement() {
-      number--;
-     // console.log(number);
+    number--;
+    //console.log(number);
 
-      $('#timeLeft').html('<h2>' + number + "seconds" + '</h2>');
+    $('#timeLeft').html('<h2>' + number + "seconds" + '</h2>');
 
-      if (number === 0) {
-        stop();//need to write a function to stop
+    if (number === 0) {
+      stop();//need to write a function to stop
 
-        $("#message").html("time up!");
-        checkAnswers();
-      }  //need to write a function to check answers
+      $("#message").html("time up!");
+      checkAnswers();
+    }  //need to write a function to check answers
 
-    };
-        //start of the game with a countdown timer
-   function run(){
-     counter = setInterval(decrement, 1000);
-       }
+  };
+  //start of the game with a countdown timer
+  function run() {
+    counter = setInterval(decrement, 1000);
+  }
 
-   function stop(){
-     clearInterval(counter);
-   }
+  function stop() {
+    clearInterval(counter);
+  }
 
-   //start of the game
+  run();
 
-   run();
+  function formTemplate(data) {
 
-   //need to  create some type of template that will allow the user to
-   //iput choices
-   function template(data) {
+    // questions can be inputed into that form field
+    var qString = "<form id='questionOne'>" + data.question + "<br>";
 
-    var string = ".form questionOne" + "_" + data.question";
     var possibles = data.possibles;
+    // a for loop to go through the possibles array
+    for (var i = 0; i < possibles.length; i++) {
+      var possible = possibles[i];
+      console.log(possible);
+      qString = qString + "<input type='radio' name='" + data.id + "' value=" + i + ">" + possible;
 
-   }
+    }
+    return qString + "</form>";
+  }
+  window.formTemplate = formTemplate;
 
-  //for (var i = 0; i < possibles.length; i++) {
-	//	var possible = possibles[i]; to check for all possibles
+  // this function takes the template created in the last function and by appending it,
+  // allows it to be displayed on the page
+  //input boxes to construct and return a query string
+  function buildQuestions() {
+    var questionHTML = 'text'
+    for (var i = 0; i < game.questions.length; i++) {
+      questionHTML = questionHTML + formTemplate(game.questions[i]);
+      console.log('questionHTML');
+    }
+    $('#questions-container').append(questionHTML);
+
+  }
+
+  // function that
+  function isCorrect(question) {
+    var answers = $('[name=' + question.id + ']');
+    var correct = answers.eq(question.answer);
+    var isChecked = correct.is(':checked');
+    return isChecked;
+  }
+
+  // call the buildQuestions function
+  buildQuestions();
+
+  // function to build the display of guesser results
+  function resultsTemplate(question) {
+    var htmlBlock = '<div>'
+    htmlBlock = htmlBlock + question.question + ': ' + isChecked;
+    return htmlBlock + "</div>";
+  }
+
+  // function to check  the guesser results
+  function checkAnswers() {
+
+    // variables needed to hold results
+    var resultsHTML = '';
+    var guessedAnswers = [];
+    var correct = 0;
+    var incorrect = 0;
+    var unAnswered = 0
+
+    // for loop iterates through each question and passes the questions at each index first into
+    // the isCorrect function to see if they match the  correct answers, and if they do,
+    // add to correct score
+    for (var i = 0; i < game.questions.length; i++) {
+      if (isCorrect(game.questions[i])) {
+        correct++;
+      } else {
+        // then this statement runs the questions at each index through the checkAnswered function
+        // to determine whether the user clicked an answer, or did not click an answer,
+        if (checkAnswered(game.questions[i])) {
+          incorrect++;
+        } else {
+          unAnswered++;
+        }
+      }
+
+    }
+    // display the results of the function in the results div and use strings of text to relate the
+    // results of the for loop with their values
+    $('.results').html('correct: ' + correct + "<br>" + 'incorrect: ' + incorrect + "<br>" + 'unanswered: ' + unAnswered);
+  }
+
+  // this function checks whether the guesser actually checked an answer
+  function checkAnswered(question) {
+    var anyAnswered = false;
+    var answers = $('[name=' + question.id + ']');
+    // the for loop creates a condition to check if the buttons were checked
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i].checked) {
+        anyAnswered = true;
+      }
+    }
+    //tell  between incorrect answers and those answers that were not attempted
+    return anyAnswered;
+
+  }
+
+  $('#doneButton').on('click', function () {
+    checkAnswers();
+    stop();
+    $("#messageDiv").html("Game Over!");
+  })
+
+
+});
